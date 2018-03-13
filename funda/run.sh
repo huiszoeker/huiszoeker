e56c00868adf0f057f683cd0c61fc1ad7d2f5c80
Henk Erik van der Hoek <mail@henkerikvanderhoek.nl>
2018-03-13 21:23:22 +0100

    ...

--- funda/run.sh
+++ funda/run.sh
@@ -3,7 +3,7 @@
 KIND=$1
 PLACE=$2
 
-CYPRESS_KIND=${KIND} CYPRESS_PLACE=${PLACE} cypress run --spec cypress/integration/gather_urls.spec.js
+CYPRESS_KIND=${KIND} CYPRESS_PLACE=${PLACE} $(npm bin)/cypress run --spec cypress/integration/gather_urls.spec.js
 
 cd server 
 KIND=$KIND PLACE=$PLACE npm run server &
@@ -19,7 +19,7 @@ do
    for i in `seq 1 3`;
    do
    	  echo $i
-   	  CYPRESS_URL=$line cypress run --spec cypress/integration/house.spec.js   
+   	  CYPRESS_URL=$line $(npm bin)/cypress run --spec cypress/integration/house.spec.js
    	  if [ "$?" == 0 ]
  	  then
         break
