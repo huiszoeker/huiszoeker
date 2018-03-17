b37b165b740d8e315fd3acaa2dc7e6d5c7072b6e
Henk Erik van der Hoek <mail@henkerikvanderhoek.nl>
2018-03-17 20:02:46 +0100

    Fix paths

--- .circleci/bigquery.sh
+++ .circleci/bigquery.sh
@@ -1,4 +1,4 @@
-openssl aes-256-cbc -d -in clientsecret.json.enc -out clientsecret.json -k ${SECRET_KEY}
+openssl aes-256-cbc -d -in .circleci/clientsecret.json.enc -out clientsecret.json -k ${SECRET_KEY}
 gcloud auth activate-service-account bq-serviceaccount@funda-195021.iam.gserviceaccount.com --key-file clientsecret.json
 
-bq load --autodetect --source_format=NEWLINE_DELIMITED_JSON funda.test ../funda/koop_utrecht_houses.json
+bq load --autodetect --source_format=NEWLINE_DELIMITED_JSON funda.test funda/koop_utrecht_houses.json
